f02ac5443e8460e64510b6e7878ffc45d16f3827
Enda Mc Cormack <endamccormack0@gmail.com>
2013-03-16 17:36:46 +0000

    changed folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ensight::Application.config.secret_token = 'fd41fae62474cab88ec7bd3b55d2325e37991b26f002b81d4e00a204455f8389b828ae7c52d9e01b931ecb79abe5d7fdd44a5da5d9bf3c64e3b3f1257217a3e1'
